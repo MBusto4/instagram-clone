d1ce4fe862eee78e12e4aaf40326cdfdd80f33ee
MBusto <mikebusto4@gmail.com>
2021-10-20 12:14:13 -0400

    css: styling using tailwind

--- instagram-clone/components/Header.js
+++ instagram-clone/components/Header.js
@@ -4,17 +4,25 @@ import Image from "next/image"
 function Header() {
     return (
         <div>
-            <h1>I am the header Comp</h1>
+            <div className="flex justify-between bg-white max-w-6xl">
+                <div className='relative hidden lg:inline-grid h-24 w-24'>
+                    <Image
+                        src='https://links.papareact.com/ocw'
+                        objectFit="contain"
+                        layout='fill'
+                    />
+                </div>
+                <div className='relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer'>
+                    <Image
+                        src='https://links.papareact.com/jjm'
+                        objectFit="contain"
+                        layout='fill'
+                    />
+                </div>
+                {/* Center */}
+                {/* Right */}
 
-            {/* Left */}
-            <div>
-                <Image
-                    src='https://links.papareact.com/ocw'
-                    layout='fill'
-                />
             </div>
-            {/* Center */}
-            {/* Right */}
         </div>
     )
 }
