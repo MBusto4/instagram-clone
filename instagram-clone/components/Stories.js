6c5b356a9da7291800ca4faf8120ab8078792b09
MBusto <mikebusto4@gmail.com>
2021-10-21 12:23:59 -0400

    feat: pulling in 20 random users from faker.js

--- instagram-clone/components/Stories.js
+++ instagram-clone/components/Stories.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import faker from 'faker'
 
 function Stories() {
+
+    //basically component did mount
+    useEffect(() => {
+        const fakeUsers = [...Array(20)].map((_, i) => ({
+
+            //generates fake info
+            ...faker.helpers.contextualCard(),
+            id: i
+        }))
+        console.log('FakeUsers--->', fakeUsers)
+    }, [])
+
+
     return (
         <div>
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+
 
         </div>
     )
--- instagram-clone/pages/index.js
+++ instagram-clone/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Header from '../components/Header'
 
 export default function Home() {
@@ -12,7 +13,7 @@ export default function Home() {
       <main className="">
         <Header />
 
-        {/* Feed */}
+        <Feed />
 
         {/* Modal */}
 
