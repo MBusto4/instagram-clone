d8572e7ddb769275b7304ab1a20aef2c4ccdf6e4
MBusto <mikebusto4@gmail.com>
2021-10-21 12:59:25 -0400

    feat: finished the stories comp

--- instagram-clone/components/Stories.js
+++ instagram-clone/components/Stories.js
@@ -18,8 +18,8 @@ function Stories() {
     }, [])
 
     return (
-        <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200
-        border rounded-sm overflow-scroll'>
+        <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-20
+        border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black'>
             {fakeUsers.map(user => (
                 <Story
                     key={user.id}
