2b75392fa4637eb6c3bc05687fc2eba07b0aee55
MBusto <mikebusto4@gmail.com>
2021-10-22 18:06:13 -0400

    feat:more styling to Modal

--- instagram-clone/components/Modal.js
+++ instagram-clone/components/Modal.js
@@ -47,7 +47,34 @@ function Modal() {
                     >
                         <div className='inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left 
                         overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full'>
-                            <h1>Hello</h1>
+                            <div>
+                                <div>
+                                    <div className='mt-3 text-center sm:mt-5'>
+                                        <Dialog.Title
+                                            as='h3'
+                                            className='text-lg leading-6 font-medium text-gray-600'
+                                        >Upload a photo</Dialog.Title>
+                                        <div>
+                                            <input type="file" hidden />
+                                        </div>
+                                        <div className='mt-2'>
+                                            <input
+                                                className='border-none focus:ring-0 w-full text-center'
+                                                type='text'
+                                                placeholder='Please Enter a caption....'
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='mt-5 sm:mt-6'>
+                                    <button
+                                        type='button'
+                                        className='inline-flex justify-center w-full rounded-md border border-transparent shadow-sm
+                                    px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-offset-2 focus:ring-2
+                                    focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disable:bg-gray-300'
+                                    >Upload</button>
+                                </div>
+                            </div>
                         </div>
                     </Transition.Child>
 
