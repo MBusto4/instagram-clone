d0c24aba5de37f88d8772f08465691cb3a662edf
MBusto <mikebusto4@gmail.com>
2021-10-22 17:46:30 -0400

    feat: working on the Modal Component

--- instagram-clone/components/Modal.js
+++ instagram-clone/components/Modal.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import { Fragment } from 'react'
 import { useRecoilState } from "recoil"
 import { modalState } from "../atoms/modalAtom"
+import { Dialog, Transition } from "@headlessui/react"
+
 
 function Modal() {
 
@@ -8,11 +10,29 @@ function Modal() {
 
 
     return (
-        <div>
-            {open && (
-                <p>modal is open</p>
-            )}
-        </div>
+        <Transition.Root show={open} as={Fragment}>
+            <Dialog
+                as='div'
+                className='fixed z-10 inset-0 overscroll-y-auto'
+                onClose={setOpen}
+            >
+                <div className='flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
+                    <Transition.Child
+                        as={Fragment}
+                        enter='ease-out duration 500ms'
+                        enterFrom='opacity-0'
+                        enterTo='opacity-100'
+                        leave='ease-in duration 300ms'
+                        leaveFrom='opacity-100'
+                        leaveTo='opacity-0'
+                    >
+                        <Dialog.Overlay className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
+                    </Transition.Child>
+
+
+                </div>
+            </Dialog>
+        </Transition.Root>
     )
 }
 
